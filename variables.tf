491765bdf5e1f7444ae528dcf97bb6113c869af3
Joanna Quaye <joanna.quaye5@gmail.com>
2023-10-31 23:29:02 +0000

    amendments made to cidr block and region

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
  type = string
  description = "aws region" 
-  default = "eu-west-2"
+  default = "eu-west-3"
 }
 
 variable "vpc_cidr" {
@@ -13,23 +13,23 @@ variable "vpc_cidr" {
 variable "public_subnet1_cidr" {
  type = string
  description = "cidr block for public subnet1" 
-  default = "10.0.1.0/24"
+  default = "10.0.1.0/28"
 }
 
 variable "public_subnet2_cidr" {
  type = string
  description = "cidr block for public subnet2" 
-  default = "10.0.2.0/24"
+  default = "10.0.2.0/28"
   }
 
 variable "private_subnet1_cidr" {
  type = string
  description = "cidr block for private subnet1" 
-  default = "10.0.3.0/24"
+  default = "10.0.3.0/28"
   }
 
 variable "private_subnet2_cidr" {
  type = string
  description = "cidr block for private subnet2" 
-  default = "10.0.4.0/24"
+  default = "10.0.4.0/28"
   }
