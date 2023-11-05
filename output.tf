35afc4dde47044edd5bac9880314896a0bf0e629
Joanna Quaye <joanna.quaye5@gmail.com>
2023-11-05 23:15:11 +0000

    amendments made to output

--- output.tf
+++ output.tf
@@ -1,25 +1,25 @@
 output "vpc_id"  {
-  value = aws_vpc.Holy-vpc.id
+  value = aws_vpc.Holy_vpc.id
 }
 
 output "vpc_cidr_block" {
-  value = aws_vpc.Holy-vpc.cidr_block
+  value = aws_vpc.Holy_vpc.cidr_block
 }
 
 output "public_subnet1_cidr" {
-  value = aws_subnet.Prod-public-subnet1.cidr_block
+  value = aws_subnet.Prod_public_subnet_1.cidr_block
 }
 
 output "private_subnet1_cidr" {
-  value = aws_subnet.Prod-private-subnet1.cidr_block
+  value = aws_subnet.Prod_private_subnet_1.cidr_block
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.Prod-public-subnet1.id
+  value = aws_subnet.Prod_public_subnet_1.id
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.Prod-private-subnet1.id
+  value = aws_subnet.Prod_private_subnet_1.id
 }
 
 
