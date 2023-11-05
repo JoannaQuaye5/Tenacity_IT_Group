142bdb0abafa3ca3939968d44dce903354f86dd6
Joanna Quaye <joanna.quaye5@gmail.com>
2023-11-05 20:06:12 +0000

    amendments made to code

--- main.tf
+++ main.tf
@@ -1,116 +1,117 @@
-resource "aws_vpc" "Holy-vpc" {
+#VPC
+resource "aws_vpc" "Holy_vpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   tags = {
-    Name = "Holy-vpc"
+    Name = "Holy_vpc"
   }
 }
 
 #public subnet1
-resource "aws_subnet" "Prod-public-subnet1" {
-  vpc_id     = aws_vpc.Holy-vpc.id
-  cidr_block = var.public_subnet1_cidr
+resource "aws_subnet" "Prod_public_subnet_1" {
+  vpc_id     = aws_vpc.Holy_vpc.id
+  cidr_block = var.public_subnet_1_cidr
   tags = {
-    Name = "Prod-public-subnet1"
+    Name = "Prod_public_subnet_1"
   }  
 }
 
 #public subnet2
-resource "aws_subnet" "Prod-public-subnet2" {
-  vpc_id     = aws_vpc.Holy-vpc.id
-  cidr_block = var.public_subnet2_cidr
+resource "aws_subnet" "Prod_public_subnet_2" {
+  vpc_id     = aws_vpc.Holy_vpc.id
+  cidr_block = var.public_subnet_2_cidr
   tags = {
-    Name = "Prod-public-subnet2"
+    Name = "Prod_public_subnet_2"
   }  
 }
 
 #private subnet1
-resource "aws_subnet" "Prod-private-subnet1" {
-  vpc_id     = aws_vpc.Holy-vpc.id
-  cidr_block = var.private_subnet1_cidr
+resource "aws_subnet" "Prod_private_subnet_1" {
+  vpc_id     = aws_vpc.Holy_vpc.id
+  cidr_block = var.private_subnet_1_cidr
   tags = {
-    Name = "Prod-private-subnet1"
+    Name = "Prod_private_subnet_1"
   }  
 }
 
 #private subnet2
-resource "aws_subnet" "Prod-private-subnet2" {
-  vpc_id     = aws_vpc.Holy-vpc.id
-  cidr_block = var.private_subnet2_cidr
+resource "aws_subnet" "Prod_private_subnet_2" {
+  vpc_id     = aws_vpc.Holy_vpc.id
+  cidr_block = var.private_subnet_2_cidr
   tags = {
-    Name = "Prod-private-subnet2"
+    Name = "Prod_private_subnet_2"
   }  
 }
 
 #aws public route table
-resource "aws_route_table" "Prod-public-route-table" {
-   vpc_id = aws_vpc.Holy-vpc.id
+resource "aws_route_table" "Prod_public_route_table" {
+   vpc_id = aws_vpc.Holy_vpc.id
    tags = {
-     Name = "Prod-public-route-table"
+     Name = "Prod_public_route_table"
    }
 }
 
 #aws private route table
-resource "aws_route_table" "Prod-private-route-table" {
-   vpc_id = aws_vpc.Holy-vpc.id
+resource "aws_route_table" "Prod_private_route_table" {
+   vpc_id = aws_vpc.Holy_vpc.id
    tags = {
-     Name = "Prod-private-route-table"
+     Name = "Prod_private_route_table"
    } 
 }
 
 #aws public route table association with public subnet 1
-resource "aws_route_table_association" "Prod-public-route-table-association" {
-  subnet_id = aws_subnet.Prod-public-subnet1.id
-  route_table_id = aws_route_table.Prod-public-route-table.id
+resource "aws_route_table_association" "Prod_public_route_table_association" {
+  subnet_id = aws_subnet.Prod_public_subnet_1.id
+  route_table_id = aws_route_table.Prod_public_route_table.id
 }
 
-#aws public route table association with public subnet 1
-resource "aws_route_table_association" "Prod-public-route-table-association1" {
-  subnet_id = aws_subnet.Prod-public-subnet1.id
-  route_table_id = aws_route_table.Prod-public-route-table.id
+#aws public route table association with public subnet 2
+resource "aws_route_table_association" "Prod_public_route_table_association1" {
+  subnet_id = aws_subnet.Prod_public_subnet_1.id
+  route_table_id = aws_route_table.Prod_public_route_table.id
   }
 
 #aws private route table association with private subnet 1
-resource "aws_route_table_association" "Prod-private-route-table-association" {
-  subnet_id = aws_subnet.Prod-private-subnet1.id
-  route_table_id = aws_route_table.Prod-private-route-table.id
+resource "aws_route_table_association" "Prod_private_route_table_association" {
+  subnet_id = aws_subnet.Prod_private_subnet_1.id
+  route_table_id = aws_route_table.Prod_private_route_table.id
 }
 
-#aws private route table association with private subnet 1
-resource "aws_route_table_association" "Prod-private-route-table-association1" {
-  subnet_id = aws_subnet.Prod-private-subnet1.id
-  route_table_id = aws_route_table.Prod-private-route-table.id
+#aws private route table association with private subnet 2
+resource "aws_route_table_association" "Prod_private_route_table_association1" {
+  subnet_id = aws_subnet.Prod_private_subnet_1.id
+  route_table_id = aws_route_table.Prod_private_route_table.id
 }
 
 #aws igw
-resource "aws_internet_gateway" "Prod-igw" {
-    vpc_id = aws_vpc.Holy-vpc.id
+resource "aws_internet_gateway" "Prod_igw" {
+    vpc_id = aws_vpc.Holy_vpc.id
     
     tags = {
-      Name = "Prod-igw"
+      Name = "Prod_igw"
     } 
 }
 #aws route for igw & public route table
-resource "aws_route" "public-internet-igw-route" {
-  route_table_id = aws_route_table.Prod-public-route-table.id
+resource "aws_route" "public_internet_igw_route" {
+  route_table_id = aws_route_table.Prod_public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.Prod-igw.id
+  gateway_id = aws_internet_gateway.Prod_igw.id
 }
 #aws nat gateway
-resource "aws_eip" "Jo-IP" {
+resource "aws_eip" "Jo_IP" {
     tags = {
-      Name = "Jo-IP"
+      Name = "Jo_IP"
     } 
 }
 #create NAT Gateway
-resource "aws_nat_gateway" "Prod-nat-gateway" {
-allocation_id = aws_eip.Jo-IP.id
-subnet_id = aws_subnet.Prod-public-subnet1.id
+resource "aws_nat_gateway" "Prod_nat_gateway" {
+allocation_id = aws_eip.Jo_IP.id
+subnet_id = aws_subnet.Prod_public_subnet_1.id
 }
 # NAT Associate with Priv route
-resource "aws_route" "private-route" {
-route_table_id = aws_route_table.Prod-private-route-table.id
-gateway_id     = aws_nat_gateway.Prod-nat-gateway.id
+resource "aws_route" "private_route" {
+route_table_id = aws_route_table.Prod_private_route_table.id
+gateway_id     = aws_nat_gateway.Prod_nat_gateway.id
 destination_cidr_block = "0.0.0.0/0"
 }
 
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
  type = string
  description = "aws region" 
-  default = "eu-west-2"
+  default = "eu_west_2"
 }
 
 variable "vpc_cidr" {
@@ -10,25 +10,25 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "public_subnet1_cidr" {
+variable "public_subnet_1_cidr" {
  type = string
  description = "cidr block for public subnet1" 
   default = "10.0.1.0/28"
 }
 
-variable "public_subnet2_cidr" {
+variable "public_subnet_2_cidr" {
  type = string
  description = "cidr block for public subnet2" 
   default = "10.0.2.0/28"
   }
 
-variable "private_subnet1_cidr" {
+variable "private_subnet_1_cidr" {
  type = string
  description = "cidr block for private subnet1" 
   default = "10.0.3.0/28"
   }
 
-variable "private_subnet2_cidr" {
+variable "private_subnet_2_cidr" {
  type = string
  description = "cidr block for private subnet2" 
   default = "10.0.4.0/28"
